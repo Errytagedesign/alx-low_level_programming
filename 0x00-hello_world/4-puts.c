605844717161461bd12de00159cb87daef897711
Sodeeq Olaide Awoyemi <errytage@gmail>
2022-09-11 10:22:47 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+Return: Always 0 (success)
+
+*/
+
+int main(void) {
+
+
+       puts("Programming is like building a multilingual puzzle '\n");
+       return (0);
+  
+
+}
