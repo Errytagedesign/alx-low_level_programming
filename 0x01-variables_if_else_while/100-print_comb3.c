14df3509cce9e9c27962485c2f629526f0aa67f1
Sodeeq Olaide Awoyemi <errytage@gmail>
2022-09-11 16:18:07 -0700

    79787

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,28 +7,26 @@
  */
 int main(void)
 {
-  int i, e;
-
-  i = 48;
-  e = 48;
-
-  while (e < 58)
-    {
-      i = 48;
-      while (i < 58)
-	{
-	  putchar(e);
-	  putchar(i);
-	  if (i == 57 && e == 57)
-	    {
-	      break;
-	    }
-	  putchar(',');
-	  putchar(' ');
-	  i++;
-	}
-      e++;
-    }
-  putchar('\n');
-  return (0);
+int i, e;
+i = 48;
+e = 48;
+while (e < 58)
+{
+i = 48;
+while (i < 58)
+{
+putchar(e);
+putchar(i);
+if (i == 57 && e == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+i++;
+}
+e++;
+}
+putchar('\n');
+return (0);
 }
