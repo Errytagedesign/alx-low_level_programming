d23d55b7ae3455d78e4a155a758f145711d63459
Sodeeq Olaide Awoyemi <errytage@gmail>
2022-09-13 15:01:26 -0700

    333

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include "Holberton"
 
 /**
  * main - Write a program that prints _putchar, followed by a new line.
  *
- * Return Always 0 (Success)
-
- */
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
