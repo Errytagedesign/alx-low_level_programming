e7650396412aa0a87cf7bc543c4c4feed291fe31
Sodeeq Olaide Awoyemi <errytage@gmail>
2022-09-13 17:20:42 -0700

    hdhd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,10 @@
  */
 void print_alphabet(void)
 {
-  int i;
-
-  for (i = 97; i <= 122; i++)
-    {
-      _putchar(i);
-    }
-  _putchar('\n');
-
+int i;
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
